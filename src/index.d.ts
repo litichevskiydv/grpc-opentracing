b484031d2232669a3446121773b6f85c3cbebeda
dmitriy.litichevskiy <litichevskiydv@gmail.com>
2020-02-11 23:55:04 +0300

    Typings were added

---
+++ src/index.d.ts
@@ -0,0 +1,20 @@
+import {
+  MethodDefinition,
+  InterceptingCall,
+  ServerUnaryCall,
+  ServerWritableStream,
+  ServerReadableStream,
+  ServerDuplexStream
+} from "grpc";
+
+interface ClientInterceptorOptions {
+  method_definition: MethodDefinition<any, any>;
+}
+
+export function clientInterceptor(options: ClientInterceptorOptions, nextCall: Function): InterceptingCall;
+
+export function serverInterceptor(
+  call: ServerUnaryCall<any> | ServerWritableStream<any> | ServerReadableStream<any> | ServerDuplexStream<any, any>,
+  methodDefinition: MethodDefinition<any, any>,
+  next: Function
+): Promise<any>;
